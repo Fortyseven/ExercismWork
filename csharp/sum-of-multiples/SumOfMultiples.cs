588ff348698f95b6cb345494faf0801998554781
Fortyseven <fortyseven@gmail.com>
2016-06-08 14:05:11 -0400

    Added SumOfMultiples solution

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class SumOfMultiples
+{
+    public static int To(int[] multiples_of, int upto)
+    {
+        int total = 0;
+
+        for (int i = 1; i < upto; i++) {
+            bool valid = false;
+            foreach (int mult in multiples_of) {
+                if ((i % mult) == 0) { valid = true; break; }
+            }
+            if (valid) {
+                total += i;
+            }
+        }
+
+        return total;
+    }
+}
