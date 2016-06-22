0b894fc658d631f1902566773182436c0cc3a797
Fortyseven <fortyseven@gmail.com>
2016-06-22 17:28:04 -0400

    Added solution for 'binary' (csharp)

---
+++ csharp/binary/Binary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Binary
+{
+    internal static int ToDecimal( string binary )
+    {
+        var result = 0;
+
+        for ( int i = 0; i < binary.Length; i++ ) {
+            if ( !Char.IsDigit( binary[ i ] ) ) return 0;
+            var digit = (int)(binary[i]- '0');
+            if ( digit > 1 ) return 0;
+            result += (int)Math.Pow( 2, binary.Length - i - 1 ) * digit;
+        }
+
+        return result;
+    }
+}
--- csharp/binary/BinaryTest.cs
+++ csharp/binary/BinaryTest.cs
@@ -5,33 +5,32 @@ public class BinaryTest
 {
     // change Ignore to false to run test case or just remove 'Ignore = true'
     [TestCase("1", ExpectedResult = 1)]
-    [TestCase("10", ExpectedResult = 2, Ignore = "Remove to run test case")]
-    [TestCase("11", ExpectedResult = 3, Ignore = "Remove to run test case")]
-    [TestCase("100", ExpectedResult = 4, Ignore = "Remove to run test case")]
-    [TestCase("1001", ExpectedResult = 9, Ignore = "Remove to run test case")]
-    [TestCase("11010", ExpectedResult = 26, Ignore = "Remove to run test case")]
-    [TestCase("10001101000", ExpectedResult = 1128, Ignore = "Remove to run test case")]
+    [TestCase("10", ExpectedResult = 2)]
+    [TestCase("11", ExpectedResult = 3)]
+    [TestCase("100", ExpectedResult = 4)]
+    [TestCase("1001", ExpectedResult = 9)]
+    [TestCase("11010", ExpectedResult = 26)]
+    [TestCase("10001101000", ExpectedResult = 1128)]
     public int Binary_converts_to_decimal(string binary)
     {
         return Binary.ToDecimal(binary);
     }
     
-    [TestCase("carrot", Ignore = "Remove to run test case")]
-    [TestCase("2", Ignore = "Remove to run test case")]
-    [TestCase("5", Ignore = "Remove to run test case")]
-    [TestCase("9", Ignore = "Remove to run test case")]
-    [TestCase("a10", Ignore = "Remove to run test case")]
-    [TestCase("100b", Ignore = "Remove to run test case")]
-    [TestCase("10c01", Ignore = "Remove to run test case")]
-    [TestCase("134678", Ignore = "Remove to run test case")]
-    [TestCase("abc10z", Ignore = "Remove to run test case")]
+    [TestCase("carrot")]
+    [TestCase("2")]
+    [TestCase("5")]
+    [TestCase("9")]
+    [TestCase("a10")]
+    [TestCase("100b")]
+    [TestCase("10c01")]
+    [TestCase("134678")]
+    [TestCase("abc10z")]
     public void Invalid_binary_is_decimal_0(string invalidValue)
     {
         Assert.That(Binary.ToDecimal(invalidValue), Is.EqualTo(0));
     }
 
-    [Ignore("Remove to run test")]
-    [Test]
+        [Test]
     public void Binary_can_convert_formatted_strings()
     {
         Assert.That(Binary.ToDecimal("011"), Is.EqualTo(3));
