04cff9116ef08ee62a90b74edbd79f05bf48e9b4
Fortyseven <fortyseven@gmail.com>
2016-07-22 15:53:54 -0400

    Added problem/solution for Allergies (chsarp)

---
+++ csharp/allergies/Allergies.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+class Allergies
+{
+    private int _score;
+
+    private Dictionary<string, int> _allergens = new Dictionary<string,int>() {
+                    {"eggs", 1},
+                    {"peanuts", 2},
+                    {"shellfish", 4},
+                    {"strawberries",8},
+                    {"tomatoes", 16},
+                    {"chocolate",32},
+                    {"pollen",64},
+                    {"cats",128}
+        };
+
+    public Allergies( int score )
+    {
+        _score = score;
+    }
+
+    public bool AllergicTo( string v )
+    {
+        int av = _allergens[v];
+
+        if ( ( _score & av ) != 0 ) return true;
+        return false;
+    }
+
+    public List<string> List()
+    {
+        var results = new List<string>();
+
+        foreach ( var al in _allergens ) {
+            if ( AllergicTo( al.Key ) ) {
+                results.Add( al.Key );
+            }
+        }
+        return results;
+    }
+}
