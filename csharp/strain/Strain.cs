cb05e60f07a16df362cda94717a74822c2bc1ef9
Fortyseven <fortyseven@gmail.com>
2016-07-22 15:53:39 -0400

    Added problem/solution for Strain (chsarp)

---
+++ csharp/strain/Strain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Extensions
+{
+    public static IEnumerable<T> Keep<T>( this IEnumerable<T> col, Func<T, bool> lam )
+    {
+        var result = from o in col
+                     where lam(o)
+                     select o;
+
+        return result;
+    }
+
+    public static IEnumerable<T> Discard<T>( this IEnumerable<T> col, Func<T, bool> lam )
+    {
+        var result = from o in col
+                     where !lam(o)
+                     select o;
+
+        return result;
+    }
+}
