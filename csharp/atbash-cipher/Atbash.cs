d5be65856d02792939b222e67c896dd901b090b8
Fortyseven <fortyseven@gmail.com>
2016-08-03 22:40:10 -0400

    Added project and solution for 'atbash-cipher' (csharp)

---
+++ csharp/atbash-cipher/Atbash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+class Atbash
+{
+    public static string Encode( string words )
+    {
+        StringBuilder result = new StringBuilder();
+        int c = 0;
+        foreach ( var ch in words.ToLower() ) {
+            bool appended = false;
+            if ( Char.IsLetter( ch ) ) {
+                result.Append( (char)( 122 - ( ( (int)ch ) - 97 ) ) );
+                appended = true;
+            }
+            else if ( Char.IsNumber( ch ) ) {
+                result.Append( ch );
+                appended = true;
+            }
+            if ( appended ) {
+                c++;
+                if ( ( c % 5 ) == 0 ) result.Append( ' ' );
+            }
+        }
+        return result.ToString().Trim();
+    }
+}
