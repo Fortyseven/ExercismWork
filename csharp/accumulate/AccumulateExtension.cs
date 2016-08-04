498e80e6a46aa1855110b5ecfeeb729a9de6b114
Fortyseven <fortyseven@gmail.com>
2016-08-04 09:04:53 -0400

    Added solution for 'accumulate' (csharp)

---
+++ csharp/accumulate/AccumulateExtension.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Extensions
+{
+    public static IEnumerable<T> Accumulate<T>( this IEnumerable<T> source, Func<T, T> callback )
+    {
+        foreach(var i in source) { yield return callback(i); }
+    }
+}
