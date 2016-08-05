1c808f6e8aea985ae98f3a8423752d4bf7a0825e
Fortyseven <fortyseven@gmail.com>
2016-08-05 12:14:26 -0400

    Added project and solution for 'trinary' (csharp)

---
+++ csharp/trinary/Trinary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Trinary // Ternary?
+{
+    private const int BASE = 3;
+
+    public static int ToDecimal( string value )
+    {
+        int result = 0;
+
+        var digits = value.ToCharArray();
+        Array.Reverse( digits );
+
+        for ( int pos = 0; pos < digits.Length; pos++ ) {
+            int digit = (int)char.GetNumericValue(digits[pos]);
+            if ( digit < 0 || digit >= BASE ) return 0;
+            result += ( digit * (int)Math.Pow( BASE, pos ));
+        }
+
+        return result;
+    }
+}
