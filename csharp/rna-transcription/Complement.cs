16a26b17b780c6d50755fa90e3e937f823fa1130
Fortyseven <fortyseven@gmail.com>
2016-08-05 12:22:22 -0400

    Added project and solution for 'rna-transcription' (csharp)

---
+++ csharp/rna-transcription/Complement.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+public class Complement
+{
+    private static char[] _dna = { 'G','C','T','A' };
+    private static char[] _rna = { 'C','G','A','U' };
+
+    public static string OfDna( string dna )
+    {
+        var result = new StringBuilder();
+        foreach(char n in dna ) {
+            for(int i = 0; i < _dna.Length; i++ ) {
+                if ( n == _dna[ i ] ) result.Append( _rna[ i ] );
+            }
+        }
+        return result.ToString();
+    }
+}
